4062150ce5f417bd44bae49e36ceca48d9393427
Nakubonga08 <nokubongangceba@gmail.com>
2023-06-15 04:14:54 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Size is not grandeur, and territory does not make a nation.
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
